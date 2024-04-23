9cb033a17aed8384e5725761463df0e3ca42189c
Bishan <tvbishan@outlook.com>
2024-04-23 21:30:32 +0530

    apiUrl

--- expense-tracker-frontend/src/services/expesneTrackerService.js
+++ expense-tracker-frontend/src/services/expesneTrackerService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const EXPENSE_TRACKER_SERVICE_URL = window.configs.apiUrl; //'http://localhost:3000/api'
+const EXPENSE_TRACKER_SERVICE_URL = '/choreo-apis/user-portal/expense-tracker-backend/expense-tracker-backend-4fd/v1.0';
 
 export const createExpense = async (expenseDetails) => {
   try {
