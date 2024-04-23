7bf47c272c5563ee21604df709f902f90644cf4b
Bishan <tvbishan@outlook.com>
2024-04-23 18:39:53 +0530

    expense tracker service initial commit with major changes

---
+++ expense-tracker-service/dbConnection.js
@@ -0,0 +1,21 @@
+const { Sequelize } = require('sequelize');
+
+// Initialize Sequelize instance
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: 'mysql', // Or your preferred database dialect
+	host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+  }
+);
+
+sequelize.authenticate().then(() => {
+  console.log('The connection has been established successfully.');
+}).catch(err => {
+  console.error('Unable to connect to the database:', err);
+});
+
+module.exports = sequelize;
